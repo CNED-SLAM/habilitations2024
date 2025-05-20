0abdeac0dfa03e44861e1bcd3fa4654d7d385818
Nathan Boudier <nathan.boudier@gmail.com>
2025-05-20 13:22:42 +0200

    Correction du contrôle du pwd

--- habilitations2024/controller/FrmHabilitationsController.cs
+++ habilitations2024/controller/FrmHabilitationsController.cs
@@ -112,15 +112,15 @@ namespace habilitations2024.controller
         /// <returns></returns>
         public bool PwdFort(string pwd)
         {
-            if (pwd.Length < 8 && pwd.Length > 30)
+            if (pwd.Length < 8 || pwd.Length > 30)
                 return false;
-            if (!Regex.Match(pwd, @"[a-z]").Success)
+            if (!Regex.Match(pwd, @"\p{Ll}").Success)
                 return false;
-            if (!Regex.Match(pwd, @"[A-Z]").Success)
+            if (!Regex.Match(pwd, @"\p{Lu}").Success)
                 return false;
             if (!Regex.Match(pwd, @"[0-9]").Success)
                 return false;
-            if (!Regex.Match(pwd, @"\W").Success)
+            if (!Regex.Match(pwd, @"[\W_]").Success)
                 return false;
             if (Regex.Match(pwd, @"\s").Success)
                 return false;
